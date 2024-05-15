33365e5804c0bbb3c528c561b8bd435d0b7b2403
shabhias <shabhialiyya3101@gmail.com>
2024-05-15 15:20:50 +0700

    add docker file

--- src/test/java/id/ac/ui/cs/advprog/heymart/customerservice/controller/HistoryControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/heymart/customerservice/controller/HistoryControllerTest.java
@@ -2,29 +2,8 @@ package id.ac.ui.cs.advprog.heymart.customerservice.controller;
 
 import id.ac.ui.cs.advprog.heymart.customerservice.model.History;
 import id.ac.ui.cs.advprog.heymart.customerservice.model.Product;
-import id.ac.ui.cs.advprog.heymart.customerservice.model.Transaction;
+
 import id.ac.ui.cs.advprog.heymart.customerservice.service.HistoryServiceImpl;
-import id.ac.ui.cs.advprog.heymart.customerservice.controller.HistoryController;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -35,8 +14,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.List;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 
 
 
